d10f506dedd4ca6fa7d5e3884b432ebf1f417c1e
contraflux <eg.rosenfeld@icloud.com>
2026-03-21 00:14:43 -0400

    Reorganize plotting

--- tensor_algebra/tensor_plotting.jl
+++ tensor_algebra/tensor_plotting.jl
@@ -3,107 +3,53 @@ using DifferentialEquations
 
 include("tensors.jl")
 
-function draw_sphere()
-    @variables θ φ
-
-    function sphere_points(u, v)
+function get_sphere(θ, φ)
+    function points(u, v)
         return [sin(u)cos(v), sin(u)sin(v), cos(u)]
     end
 
-    sphere_basis = [
+    basis = [
         Tensor([cos(θ)cos(φ), cos(θ)sin(φ), -sin(θ)]),
         Tensor([-sin(θ)sin(φ), sin(θ)cos(φ), 0])
     ]
-
+    
     us = range(1e-2, π - 1e-2, 50)
     vs = range(0, 2π, 50)
 
-    Γ = christoffel((θ, φ), sphere_basis)
-    function geodesic!(du, u, p, t)
-        # u = [θ, φ, vθ, vφ]
-        # du = derivatives of u
-        Γ_num = substitute(Γ, Dict(θ=>u[1], φ=>u[2]))
-        du[1] = u[3]
-        du[2] = u[4]
-        du[3] = -sum([Γ_num[1][i,j] * u[2+i] * u[2+j] for i in 1:2, j in 1:2])
-        du[4] = -sum([Γ_num[2][i,j] * u[2+i] * u[2+j] for i in 1:2, j in 1:2])
-    end
+    Γ = christoffel((θ, φ), basis)
+    R_scalar = ricci_scalar((θ, φ), basis)
 
-    u0 = [π/2, 0.0, 1.0, -1.0]
-    tspan = (0.0, 5.0)
-    prob = ODEProblem(geodesic!, u0, tspan)
-    sol = solve(prob, abstol=1e-10, reltol=1e-10)
-
-    geodesic_points = [sphere_points(s[1], s[2]) for s in sol.u]
-    R_scalar = ricci_scalar((θ, φ), sphere_basis)
-
-    cartesian_points = [sphere_points(u, v) for u in us, v in vs]
-    R_scalar_values = [Float64(Symbolics.unwrap(substitute(R_scalar, Dict(θ=>u, φ=>v)))) for u in us, v in vs]
-
-    plot_shape(cartesian_points, R_scalar_values, geodesic_points)
+    return points, basis, (us, vs), Γ, R_scalar
 end
 
-function draw_torus(R=3, r=1)
-    @variables θ φ
-
-    function torus_points(u, v)
+function get_torus(θ, φ, R=3, r=1)
+    function points(u, v)
         return [(R + r * cos(u)) * cos(v), (R + r * cos(u)) * sin(v), r * sin(u)]
     end
 
-    torus_basis = [
+    basis = [
         Tensor([-r * sin(θ) * cos(φ), -r * sin(θ) * sin(φ), r * cos(θ)]),
         Tensor([-(R + r * cos(θ)) * sin(φ), (R + r * cos(θ)) * cos(φ), 0])
     ]
 
-
     us = range(0, 2π, 50)
     vs = range(0, 2π, 50)
 
-    Γ = christoffel((θ, φ), torus_basis)
-    function geodesic!(du, u, p, t)
-        # u = [θ, φ, vθ, vφ]
-        # du = derivatives of u
-        Γ_num = substitute(Γ, Dict(θ=>u[1], φ=>u[2]))
-        du[1] = u[3]
-        du[2] = u[4]
-        du[3] = -sum([Γ_num[1][i,j] * u[2+i] * u[2+j] for i in 1:2, j in 1:2])
-        du[4] = -sum([Γ_num[2][i,j] * u[2+i] * u[2+j] for i in 1:2, j in 1:2])
-    end
+    Γ = christoffel((θ, φ), basis)
+    R_scalar = ricci_scalar((θ, φ), basis)
 
-    u0 = [0.0, 0.0, 3.25, -0.5]
-    tspan = (0.0, 5.0)
-    prob = ODEProblem(geodesic!, u0, tspan)
-    sol = solve(prob, abstol=1e-10, reltol=1e-10)
-
-    geodesic_points = [torus_points(s[1], s[2]) for s in sol.u]
-    R_scalar = ricci_scalar((θ, φ), torus_basis)
-
-    cartesian_points = [torus_points(u, v) for u in us, v in vs]
-    R_scalar_values = [Float64(Symbolics.unwrap(substitute(R_scalar, Dict(θ=>u, φ=>v)))) for u in us, v in vs]
-
-    coarse_us = us[begin:4:end]
-    coarse_vs = vs[begin:2:end]
-    grid = [torus_points(u, v) for u in coarse_us, v in coarse_vs]
-    vecs = [
-        sin(u/2 + v) * substitute(torus_basis[1], Dict(θ=>u, φ=>v)).data + 
-        cos(3v - 2) * substitute(torus_basis[2], Dict(θ=>u, φ=>v)).data 
-        for u in coarse_us, v in coarse_vs
-    ]
-
-    plot_shape(cartesian_points, R_scalar_values, geodesic_points, (grid, vecs))
+    return points, basis, (us, vs), Γ, R_scalar
 end
 
-function draw_klein(r=3)
-    @variables θ φ
-
-    function klein_points(u, v)
+function get_klein(θ, φ, r=3)
+    function points(u, v)
         x = (r + cos(u/2)*sin(v) - sin(u/2)*sin(2v)) * cos(u)
         y = (r + cos(u/2)*sin(v) - sin(u/2)*sin(2v)) * sin(u)
         z = sin(u/2)*sin(v) + cos(u/2)*sin(2v)
         return [x, y, z]
     end
 
-    klein_basis = [
+    basis = [
         Tensor([
             (-sin(θ/2)/2 * sin(φ) - cos(θ/2)/2 * sin(2φ)) * cos(θ) - (r + cos(θ/2)*sin(φ) - sin(θ/2)*sin(2φ))*sin(θ),
             (-sin(θ/2)/2 * sin(φ) - cos(θ/2)/2 * sin(2φ)) * sin(θ) + (r + cos(θ/2)*sin(φ) - sin(θ/2)*sin(2φ))*cos(θ),
@@ -119,66 +65,82 @@ function draw_klein(r=3)
     us = range(0, 2π, 50)
     vs = range(0, 2π, 50)
 
-    Γ = christoffel((θ, φ), klein_basis)
-    function geodesic!(du, u, p, t)
-        # u = [θ, φ, vθ, vφ]
-        # du = derivatives of u
-        Γ_num = substitute(Γ, Dict(θ=>u[1], φ=>u[2]))
-        du[1] = u[3]
-        du[2] = u[4]
-        du[3] = -sum([Γ_num[1][i,j] * u[2+i] * u[2+j] for i in 1:2, j in 1:2])
-        du[4] = -sum([Γ_num[2][i,j] * u[2+i] * u[2+j] for i in 1:2, j in 1:2])
-    end
+    Γ = christoffel((θ, φ), basis)
+    R_scalar = ricci_scalar((θ, φ), basis)
 
-    u0 = [0.0, 0.0, 3.25, -0.5]
-    tspan = (0.0, 5.0)
-    prob = ODEProblem(geodesic!, u0, tspan)
-    sol = solve(prob, abstol=1e-10, reltol=1e-10)
-
-    geodesic_points = [klein_points(s[1], s[2]) for s in sol.u]
-    R_scalar = ricci_scalar((θ, φ), klein_basis)
-
-    cartesian_points = [klein_points(u, v) for u in us, v in vs]
-    R_scalar_values = [Float64(Symbolics.unwrap(substitute(R_scalar, Dict(θ=>u, φ=>v)))) for u in us, v in vs]
-
-    plot_shape(cartesian_points, R_scalar_values, geodesic_points)
+    return points, basis, (us, vs), Γ, R_scalar
 end
 
-function plot_shape(points, scalar_field, geodesic_points=[], vector_field=())
+function geodesic!(Γ, du, u, p, t)
+    # u = [θ, φ, vθ, vφ]
+    # du = derivatives of u
+    Γ_num = substitute(Γ, Dict(θ=>u[1], φ=>u[2]))
+    du[1] = u[3]
+    du[2] = u[4]
+    du[3] = -sum([Γ_num[1][i,j] * u[2+i] * u[2+j] for i in 1:2, j in 1:2])
+    du[4] = -sum([Γ_num[2][i,j] * u[2+i] * u[2+j] for i in 1:2, j in 1:2])
+end
+
+function plot_surface!(ax, points, values)
     x = [p[1] for p in points]
     y = [p[2] for p in points]
     z = [p[3] for p in points]
-    color_limit = maximum(abs.(scalar_field))
-
-    # Setup axis
-    set_theme!(theme_dark())
-    fig = Figure(size=(700, 500), fxaa=true)
-    ax = Axis3(fig[1,1], aspect=:data)
-    ax.title = "Ricci Scalar Curvature"
-    hidespines!(ax)
-
-    # Plot surface
+    color_limit = maximum(abs.(values))
     wireframe!(ax, x, y, z, color=:white, linewidth=0.25, alpha=0.5)
-    s = surface!(ax, x, y, z, color=scalar_field, colormap=:RdBu, colorrange=(-color_limit, color_limit))
-    Colorbar(fig[1,2], s)
-
-    # Plot geodesic
-    if !isempty(geodesic_points)
-        xs = [p[1] for p in geodesic_points]
-        ys = [p[2] for p in geodesic_points]
-        zs = [p[3] for p in geodesic_points]
-        lines!(ax, xs, ys, zs, color=:lightblue, linewidth=2)
-    end
-
-    # Plot vectors
-    if !isempty(vector_field)
-        grid = vec([Point3f(x) for x in vector_field[1]])
-        vecs = vec([Vec3f(v) for v in vector_field[2]])
-        lengths = vec([norm(v) for v in vector_field[2]])
-        arrows!(ax, grid, vecs, color=lengths, colormap=:magma, arrowsize=0.025, lengthscale=0.05)
-    end
-
-    display(fig)
+    s = surface!(ax, x, y, z, color=values, colormap=:RdBu, colorrange=(-color_limit, color_limit))
+    return s
 end
 
-draw_torus()
+function plot_line!(ax, points)
+    xs = [p[1] for p in points]
+    ys = [p[2] for p in points]
+    zs = [p[3] for p in points]
+    lines!(ax, xs, ys, zs, color=:lightblue, linewidth=2)
+end
+
+function plot_vectors!(ax, grid, vecs)
+    grid = vec([Point3f(x) for x in grid])
+    vecs = vec([Vec3f(v) for v in vecs])
+    lengths = vec([norm(v) for v in vecs])
+    arrows!(ax, grid, vecs, color=lengths, colormap=:magma, arrowsize=0.025, lengthscale=0.1)
+end
+
+@variables θ φ
+
+set_theme!(theme_dark())
+fig = Figure(size=(700, 500), fxaa=true)
+ax = Axis3(fig[1,1], aspect=:data)
+ax.title = "Ricci Scalar Curvature"
+hidespines!(ax)
+
+points, basis, (us, vs), Γ, R_scalar = get_klein(θ, φ)
+
+# Surface points
+cartesian_points = [points(u, v) for u in us, v in vs]
+scalar_field = [Float64(Symbolics.unwrap(substitute(R_scalar, Dict(θ=>u, φ=>v)))) for u in us, v in vs]
+
+# Geodesic
+u0 = [π/2, 0.0, 1.0, -1.0]
+tspan = (0.0, 5.0)
+probem = ODEProblem((du, u, p, t) -> geodesic!(Γ, du, u, p, t), u0, tspan)
+solution = solve(prob, abstol=1e-10, reltol=1e-10)
+geodesic_points = [points(u_t[1], u_t[2]) for u_t in solution.u]
+
+# Vector field
+coarse_us = us[begin:2:end]
+coarse_vs = vs[begin:2:end]
+grid = [points(u, v) for u in coarse_us, v in coarse_vs]
+vecs = [
+    substitute((
+        sin(v) * basis[1][:i] + 
+        cos(u) * basis[2][:i]
+    ).tensor, Dict(θ=>u, φ=>v)).data
+    for u in coarse_us, v in coarse_vs
+]
+
+s = plot_surface!(ax, cartesian_points, scalar_field)
+Colorbar(fig[1,2], s)
+plot_line!(ax, geodesic_points)
+plot_vectors!(ax, grid, vecs)
+
+display(fig)
